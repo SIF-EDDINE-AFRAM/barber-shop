42c6e7456f00fc9d71788c5915906563ea00b201
majed iblalen <majdiblalen.2020@gmail.com>
2025-02-10 21:12:52 +0100

    authentification

--- server/app.mjs
+++ server/app.mjs
@@ -1,10 +1,12 @@
 import express from "express"
 import router from "./router.mjs"
+import dotenv from 'dotenv'
+import bodyParser from "body-parser"
+dotenv.config()
 
 const app = express()
-
-app.listen(1999, ()=>{
-    console.log("server live on ", 1999)
-})
-
+app.use(bodyParser.json())
 app.use(router)
+app.listen(process.env.PORT,()=>{
+    console.log("server live on ",process.env.PORT)
+})
--- server/router.mjs
+++ server/router.mjs
@@ -1,8 +1,8 @@
 import express from "express"
-const router = express.Router()
-
-router.get("/", (req, res)=>{
-    res.send("a")
-})
+import Login from "./controllers.mjs"
+import { verifyToken } from "./controllers.mjs"
+const router = express.Router() 
+       router.post('/login',Login)
+    router.get('/haha',verifyToken,(req,res)=>res.send('Sif Lkelb'))
 
 export default router
